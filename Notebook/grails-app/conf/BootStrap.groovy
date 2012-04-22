947dc565889175ccae94a8963b516d7d1ef490ad
Your Name <Josueore@hotmail.co.uk>
2012-04-22 14:19:51 +0100

    created Students Domain object, generated scaffolds,Added test data to BootStrap

--- Notebook/grails-app/conf/BootStrap.groovy
+++ Notebook/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,12 @@
+import notebook.Student
 class BootStrap {
 
     def init = { servletContext ->
+   def Josuestu = new Student(name:'Josue',courseCode:'439 247').save();
+   def Linostu = new Student(name:'Lino',courseCode:'286 279').save();
+   def Alexstu = new Student(name:'Alex',courseCode:'284 303').save();
     }
+    
     def destroy = {
     }
 }
